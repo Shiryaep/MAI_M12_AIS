3402cb04570e9cc3a82cd171187a8ba889872fdc
ShiryaeP <shiryaep@gmail.com>
2023-04-27 13:01:52 +0300

    Сделаны изменения для получения языка в качестве параметра

--- weather_servise_flask.py
+++ weather_servise_flask.py
@@ -6,7 +6,14 @@ app = Flask(__name__)
 
 @app.route('/query-example')
 def query_example():
-    return 'Query String Example'
+    # if key doesn't exist, returns None
+    #you can also use lang = request.args['language']
+    # if key doesn't exist, second way returns a 400 error
+    #language = request.args.get('language')
+    language = 'hey'
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
+    #return 'Query String Example'
 
 @app.route('/form-example')
 def form_example():
