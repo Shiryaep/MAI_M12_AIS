fb4bd76173134c7794783d2cece7187d5b6ba2e0
ShiryaeP <shiryaep@gmail.com>
2023-04-27 16:28:12 +0300

    Добьавлен гитигнор, во фласке дописано получение погоды, в погоде дописана функция получения погоды

--- weather_servise.py
+++ weather_servise.py
@@ -1,19 +1,26 @@
 import urllib.request
 
-city = input()
+def getWeatherByCity(city):
+    getStr = "http://wttr.in/" + city + "?format=\"" + "%l:%t\""
+    contents = urllib.request.urlopen(getStr).read()
+    return contents.decode('utf-8', 'ignore')
 
-#contents = urllib.request.urlopen("http://google.com").read()
-getString = "http://wttr.in/" + city + "?format=\""
-getStringCorrectFormat = getString + "%l:%t\""
-getString += "%l:+%C+%t+%p+%T\""
 
-#print(getString)
+if (__name__ == "__main__"):
+    city = input()
 
-contents = urllib.request.urlopen(getString).read()
-#print('6\xc2\xb0C')
-print(contents.decode('utf-8', 'ignore'))
+    #contents = urllib.request.urlopen("http://google.com").read()
+    getString = "http://wttr.in/" + city + "?format=\""
+    getStringCorrectFormat = getString + "%l:%t\""
+    getString += "%l:+%C+%t+%p+%T\""
 
-print("\nCORRECT FORMAT")
-contents = urllib.request.urlopen(getStringCorrectFormat).read()
-#print('6\xc2\xb0C')
-print(contents.decode('utf-8', 'ignore'))
+    #print(getString)
+
+    contents = urllib.request.urlopen(getString).read()
+    #print('6\xc2\xb0C')
+    print(contents.decode('utf-8', 'ignore'))
+
+    print("\nCORRECT FORMAT")
+    contents = urllib.request.urlopen(getStringCorrectFormat).read()
+    #print('6\xc2\xb0C')
+    print(contents.decode('utf-8', 'ignore'))
--- weather_servise_flask.py
+++ weather_servise_flask.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import urllib.request
+import weather_servise as ws
 
 
 app = Flask(__name__)
@@ -11,17 +12,11 @@ def query_example(city, dt):
     # if key doesn't exist, second way returns a 400 error
     #language = request.args.get('city')
     #language = 'hey'
+    weather = ws.getWeatherByCity(city)
 
-    return '''<h1>The language value is: {}</h1>'''.format(city)
+    return '''Weather is {}'''.format(weather)
     #return 'Query String Example'
 
-@app.route('/form-example')
-def form_example():
-    return 'Form Data Example'
-
-@app.route('/json-example')
-def json_example():
-    return 'JSON Object Example'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5015)
