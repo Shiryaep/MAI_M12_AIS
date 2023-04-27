05d494540b998d37068e8f5c5cf90812ee77d442
ShiryaeP <shiryaep@gmail.com>
2023-04-27 12:48:23 +0300

    Изменены значения в туду, создан пример на flask и немного доработан сервис получения погоды

--- weather_servise.py
+++ weather_servise.py
@@ -1,14 +1,23 @@
 import urllib.request
 
+city = input()
+
 #contents = urllib.request.urlopen("http://google.com").read()
-getString = "http://wttr.in/Vancouver?format="
+getString = "http://wttr.in/"
+getString += city + "?format="
 getString += "\""
+getStringCorrectFormat = getString + "%l:%t"
 getString += "%l:+%C+%t+%p+%T"
 getString += "\""
+getStringCorrectFormat +=  "\""
 
-print(getString)
+#print(getString)
 
 contents = urllib.request.urlopen(getString).read()
+#print('6\xc2\xb0C')
+print(contents.decode('utf-8', 'ignore'))
 
+print("\nCORRECT FORMAT")
+contents = urllib.request.urlopen(getStringCorrectFormat).read()
 #print('6\xc2\xb0C')
 print(contents.decode('utf-8', 'ignore'))
