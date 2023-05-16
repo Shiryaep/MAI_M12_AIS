3b8d1403dcf63df9a610a6650ef820eee3bc3f44
ShiryaeP <shiryaep@gmail.com>
2023-05-16 22:14:38 +0300

    Доделана игра в угадайку

--- Guess_game/Client.py
+++ Guess_game/Client.py
@@ -1,11 +1,45 @@
 import socket
 
 HOST = "127.0.1.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 5017  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+def printRules():
+    print("This is simple guess game for numbers from 0 to 100")
+    print("You will present number and server will answer")
+    print("[more] if your number less then correct")
+    print("[less] if your number more then correct")
+    print("Lets get started!")
+    print()
 
-print(f"Received {data!r}")
+
+def getAnswer():
+    isOk = False
+    while(not(isOk)):
+        print("Try your number", end=' :')
+        inputNum = int(input())
+        if (inputNum > 100) or (inputNum < 0):
+            print("READ RULES!!!!!!!!!!!!!!!!!!!")
+            printRules()
+            isOk = False
+        else:
+            isOk = True
+    return inputNum
+
+def decodeData(data):
+    return data.decode()
+
+if (__name__ == "__main__"):
+    printRules()
+    num = getAnswer()
+    count = 1
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(str(num).encode())
+        data = s.recv(1024)
+        while (decodeData(data) != "equal"):
+            print("Server says: " + decodeData(data))
+            num = getAnswer()
+            s.sendall(str(num).encode())
+            data = s.recv(1024)
+            count+=1
+    print(f"Great shot! Used " + str(count) + " attempt(s)")
--- Guess_game/Server.py
+++ Guess_game/Server.py
@@ -1,16 +1,39 @@
 import socket
+import random
 
 HOST = "127.0.1.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+PORT = 5017  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+def nextMove(curNum, corrNum):
+    if(curNum == corrNum):
+        return "equal"
+    if(curNum < corrNum):
+        return "more"
+    if(curNum > corrNum):
+        return "less"
+
+def getIntData(data):
+    retValue = data.decode()
+    return int(retValue)
+
+def getCodeData(answer):
+    return answer.encode()
+
+if (__name__ == "__main__"):
+    while (True):
+        correctNumber = random.randint(0, 100)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                print(correctNumber)
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    retValue = getIntData(data)
+                    retValue = nextMove(retValue, correctNumber)
+                    retValue = getCodeData(retValue)
+                    conn.sendall(retValue)
